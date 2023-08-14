e75329936a7f947393252429f4c68da7ecf96b93
jnarwell <jcmarwell@gmail.com>
2023-08-14 13:19:03 -0700

    added base template

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,6 @@ def index():
     countries = ['China', 'India', 'Canada']
     return render_template('index.html', first_name='Jamie', countries = countries)
 
-@app.route('/new')
+@app.route('/signup')
 def new():
-    return f'This is a new route'
+    return render_template('signup.html')
