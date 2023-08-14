e6271b498645ac70e489f1d29edd87023f6b8c46
jnarwell <jcmarwell@gmail.com>
2023-08-14 12:46:22 -0700

    added navbar

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
+from flask import render_template
 
 #add a route
 @app.route('/')
 def index():
-    return 'Hello World'
+    countries = ['China', 'India', 'Canada']
+    return render_template('index.html', first_name='Jamie', countries = countries)
 
 @app.route('/new')
 def new():
