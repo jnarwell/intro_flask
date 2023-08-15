b416308eaebb8e0cea0d23f6b0d1231e24878564
jnarwell <jcmarwell@gmail.com>
2023-08-15 12:08:29 -0700

    added form settings

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import SignUpForm
 
 # Add a route
@@ -7,7 +7,18 @@ from app.forms import SignUpForm
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
     form = SignUpForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(first_name, last_name, username, email, password)
+
+        #redirect to home page
+        return redirect(url_for('index'))
+
     return render_template('signup.html', form=form)
