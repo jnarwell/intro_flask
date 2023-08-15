f1f88dbdd73e419bcca4ff48633571770fa3a32f
jnarwell <jcmarwell@gmail.com>
2023-08-15 13:14:28 -0700

    database start

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #create instance
 app = Flask(__name__)
 #configure app with secret key
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config.from_object(Config)
 
+#create instance of database 
+db = SQLAlchemy(app)
+
+#create instance of migrate to handle database migrations
+migrate = Migrate(app, db)
 
 from app import routes
 #must be imported at bottom of file
